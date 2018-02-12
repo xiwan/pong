cc5b7feb8c617aee71ed1b4a7847229d24e02d69
xiwan <5155280@qq.com>
2018-02-12 17:19:44 +0800

    pong game

---
+++ Assets/script/MoveRacket.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket : MonoBehaviour {
+	public float speed = 30;
+	public string axis = "Vertical";
+
+	public void FixedUpdate () {
+		float v = Input.GetAxisRaw (axis);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, v).normalized * 30;
+	}
+}
